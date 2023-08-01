3496ddea4280abf69886abbfb7e02dc9ae8bbf65
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-01 14:35:50 +0000

    segunda entrega intermedia

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-var pronoun = ["the", "our"];
-var adj = ["great", "big"];
-var noun = ["jogger", "racoon"];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let xtends = [".com", ".net", ".us", ".io"];
+let doms = [];
+let links = [];
 
-for (let p = 0; p < pronoun.length; p++) {
-  for (let a = 0; a < adj.length; a++) {
-    for (let n = 0; n < noun.length; n++) {
-      console.log(pronoun[p] + adj[a] + noun[n] + ".com");
+function gen_doms() {
+  for (let pronounS = 0; pronounS < pronoun.length; pronounS++) {
+    for (let adjS = 0; adjS < adj.length; adjS++) {
+      for (let nounS = 0; nounS < noun.length; nounS++) {
+        let dom = pronoun[pronounS] + adj[adjS] + noun[nounS];
+        doms.push(dom);
+        // console.log(dom);
+        // return dom;
+      }
     }
   }
 }
+
+function add_extend() {
+  for (let dom = 0; dom < doms.length; dom++) {
+    for (let extend = 0; extend < xtends.length; extend++) {
+      let link = "https://" + doms[dom] + xtends[extend];
+      links.push(link);
+    }
+  }
+}
+
+gen_doms();
+add_extend();
+console.log(links);
